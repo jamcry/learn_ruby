05f819a264659436bf91f20548e6ad7b86ba56a3
jamcry <jamcry@hotmail.com>
2019-03-06 22:02:27 +0300

    Completed exercise 05.

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
 # write your code here
+    attr_accessor :title  
+    
+    def title
+        words = @title.downcase.split(" ")
+        little_words = ["and","or","the","over","a","an","in","of"]
+
+        (0...words.length).each do |i|
+            word = words[i]
+            if (little_words.include? word) and (i != 0)
+                words[i] = word.downcase
+            else
+                words[i] = word.capitalize
+            end
+        end
+
+        caps_title = words.join(" ")
+        return caps_title
+    end
 end
