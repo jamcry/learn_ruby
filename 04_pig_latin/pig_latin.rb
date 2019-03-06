a448fd7966db189df272ddb4f9e2571f0902f22b
jamcry <jamcry@hotmail.com>
2019-03-06 21:50:59 +0300

    Completed exercise 04.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,29 @@
 #write your code here
+def translate(s)
+    words = s.split(" ")
+    vowels = ["a","e","i","o","u"]
+    tr_words = []
+    words.each do |w|
+        if vowels.include? w[0]
+            tr_words << w + "ay"
+        else #if starts with a consonant
+            if w.include? "qu"
+                i = w.index("qu")
+                if i == 0
+                    w = w.slice(i+2,w.length-1) + "quay"
+                else
+                    w = w.slice(i+2,w.length-1) + (w.slice(0,i+2) + "ay")    
+                end
+            else
+                until vowels.include? w[0]
+                    w += w[0]
+                    w[0] = ""
+                end
+                w += "ay"
+            end
+            
+            tr_words << w
+        end
+    end
+    return tr_words.join(" ")
+end
