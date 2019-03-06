2c533dc6e64fe48d9ec81f841a3965b914c2c29a
jamcry <jamcry@hotmail.com>
2019-03-06 23:06:06 +0300

    Completed exercise 06.

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,31 @@
 class Timer
   #write your code here
+  attr_accessor :seconds, :time_string
+
+  def initialize
+    @seconds = 0
+    @time_string = get_time_string
+  end
+
+  def seconds=(secs)
+    @seconds = secs
+    @time_string = get_time_string
+    return @seconds
+  end
+
+  def get_time_string
+    mins = @seconds / 60
+    secs = @seconds % 60
+    hrs = mins / 60
+    mins = mins % 60
+    hrs = padded(hrs)
+    mins = padded(mins)
+    secs = padded(secs)
+    return "#{hrs}:#{mins}:#{secs}"
+  end
+
+  def padded(num)
+    num = "0"+num.to_s if num < 10
+    return num
+  end
 end
